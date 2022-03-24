1827c9843c50c947f4415811adab31db0008464f
1Shmel1 <aninom4ik@gmail.com>
2022-03-24 17:53:05 +0300

    edit Procfile

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request
 import logging
 import json
-import requests
-import os
-from math import sin, cos, sqrt, atan2, radians
 from geo import get_country, get_distance, get_coordinates
 
 app = Flask(__name__)
@@ -73,61 +70,5 @@ def get_cities(req):
 
     return cities
 
-
-def get_coordinates(city):
-    url = "https://geocode-maps.yandex.ru/1.x/"
-
-    params = {
-        'geocode': city,
-        'format': 'json',
-        'apikey': "40d1649f-0493-4b70-98ba-98533de7710b"
-    }
-
-    response = requests.get(url, params)
-    json = response.json()
-    point_str = json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-    point_array = [float(x) for x in point_str.split(' ')]
-
-    return point_array
-
-
-def get_country(city):
-    url = "https://geocode-maps.yandex.ru/1.x/"
-
-    params = {
-        'geocode': city,
-        'format': 'json',
-        'apikey': "40d1649f-0493-4b70-98ba-98533de7710b"
-    }
-
-    response = requests.get(url, params)
-    json = response.json()
-
-    return \
-        json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
-            'GeocoderMetaData'][
-            'AddressDetails']['Country']['CountryName']
-
-
-def get_distance(p1, p2):
-    R = 6373.0
-
-    lon1 = radians(p1[0])
-    lat1 = radians(p1[1])
-    lon2 = radians(p2[0])
-    lat2 = radians(p2[1])
-
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-    distance = R * c
-
-    return distance
-
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
